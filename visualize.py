16e87cfe9c9f04ab3f5d566e762b1b1ef1e791c2
eugnes03 <eugennesbakken@gmail.com>
2026-03-17 14:47:19 -0400

    Simple visualization script, for local development and checking results

---
+++ visualize.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("build/paths.csv")  
+time = df.iloc[:, 0]
+
+paths = df.iloc[:, 1:]
+
+plt.figure()
+
+for col in paths.columns:
+    plt.plot(time, paths[col], label=col)
+
+plt.xlabel("Time")
+plt.ylabel("Value")
+plt.title("Paths over Time")
+
+plt.legend()
+
+plt.show()
